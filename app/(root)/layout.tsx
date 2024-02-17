0a02c0d8c3fa494979692ae26c984d830927c7fb
Pruthvisinh Rajput <pruthvirajput97@gmail.com>
2024-02-17 12:48:16 +0530

    Add Sidebar component to RootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,12 @@
+import Sidebar from "@/components/shared/Sidebar";
 import React from "react";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
-      <div className="root">
-        <div className="root-container">
-          <div className="wrapper">{children}</div>
-        </div>
+      <Sidebar />
+      <div className="root-container">
+        <div className="wrapper">{children}</div>
       </div>
     </main>
   );
